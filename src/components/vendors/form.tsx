081fd1cafb55b659629da7277456ac3db96563d3
Mohamed Ali Missaoui <mam@missaoui.org>
2026-07-26 09:24:56 +0200

    feat: convert vendor drawer to two-column card layout

--- src/components/vendors/form.tsx
+++ src/components/vendors/form.tsx
@@ -1,6 +1,19 @@
 import { useEffect, useMemo, useState } from "react";
 import { useLocation, useNavigate } from "react-router";
-import { Button, Drawer, Form, Input, InputNumber, Popconfirm, Select, Space, Typography } from "antd";
+import {
+  Button,
+  Card,
+  Col,
+  Drawer,
+  Form,
+  Input,
+  InputNumber,
+  Popconfirm,
+  Row,
+  Select,
+  Space,
+  Typography,
+} from "antd";
 import { useAtom, useAtomValue, useSetAtom } from "jotai";
 import { Trans } from "@lingui/react/macro";
 import { t } from "@lingui/core/macro";
@@ -13,7 +26,6 @@ import { vendorIdAtom, vendorAtom, vendorsAtom, deleteVendorAtom } from "src/ato
 import { generateClientCode } from "src/utils/client";
 import { currencies } from "src/utils/currencies";
 import ScrollShadow from "src/components/scroll-shadow";
-import Section from "src/components/form-section";
 
 const VendorForm = () => {
   const location = useLocation();
@@ -103,7 +115,7 @@ const VendorForm = () => {
       title={vendorId ? <Trans>Edit vendor</Trans> : <Trans>New vendor</Trans>}
       open={isVisible}
       placement="right"
-      size={480}
+      size={640}
       onClose={handleClose}
       footer={
         <div style={{ display: "flex", justifyContent: "space-between" }}>
@@ -138,49 +150,80 @@ const VendorForm = () => {
     >
       <ScrollShadow>
         <Form form={form} layout="vertical" onFinish={handleSubmit}>
-          <Section><Trans>Contact</Trans></Section>
-          <Form.Item name="name" label={<Trans>Name</Trans>} rules={[{ required: true, message: t`Please input name!` }]}>
-            <Input
-              placeholder={t`Name`}
-              onChange={(e) => {
-                if (!vendorId) form.setFieldValue("code", generateClientCode(e.target.value));
-              }}
-            />
-          </Form.Item>
-          <Form.Item name="code" label={<Trans>Code</Trans>}>
-            <Input placeholder={t`Code`} maxLength={10} />
-          </Form.Item>
-          <Form.Item name="emails" label={<Trans>E-mails</Trans>}>
-            <Select placeholder={t`E-mails`} mode="tags" tokenSeparators={[",", ";"]} />
-          </Form.Item>
-          <Form.Item name="phone" label={<Trans>Phone</Trans>}>
-            <Input placeholder={t`Phone`} />
-          </Form.Item>
-          <Form.Item name="website" label={<Trans>Website</Trans>}>
-            <Input placeholder={t`Website`} />
-          </Form.Item>
-          <Form.Item name="vatin" label={<Trans>VAT Number</Trans>}>
-            <Input placeholder={t`VAT Number`} />
-          </Form.Item>
+          <Card title={<Trans>Contact</Trans>} style={{ marginBottom: 12 }}>
+            <Row gutter={[16, 0]}>
+              <Col xs={24}>
+                <Form.Item
+                  name="name"
+                  label={<Trans>Name</Trans>}
+                  rules={[{ required: true, message: t`Please input name!` }]}
+                >
+                  <Input
+                    placeholder={t`Name`}
+                    onChange={(e) => {
+                      if (!vendorId) form.setFieldValue("code", generateClientCode(e.target.value));
+                    }}
+                  />
+                </Form.Item>
+              </Col>
+              <Col xs={24} md={12}>
+                <Form.Item name="code" label={<Trans>Code</Trans>}>
+                  <Input placeholder={t`Code`} maxLength={10} />
+                </Form.Item>
+              </Col>
+              <Col xs={24} md={12}>
+                <Form.Item name="vatin" label={<Trans>VAT Number</Trans>}>
+                  <Input placeholder={t`VAT Number`} />
+                </Form.Item>
+              </Col>
+              <Col xs={24}>
+                <Form.Item name="emails" label={<Trans>E-mails</Trans>}>
+                  <Select placeholder={t`E-mails`} mode="tags" tokenSeparators={[",", ";"]} />
+                </Form.Item>
+              </Col>
+              <Col xs={24} md={12}>
+                <Form.Item name="phone" label={<Trans>Phone</Trans>}>
+                  <Input placeholder={t`Phone`} />
+                </Form.Item>
+              </Col>
+              <Col xs={24} md={12}>
+                <Form.Item name="website" label={<Trans>Website</Trans>}>
+                  <Input placeholder={t`Website`} />
+                </Form.Item>
+              </Col>
+              <Col xs={24}>
+                <Form.Item name="address" label={<Trans>Address</Trans>} style={{ marginBottom: 0 }}>
+                  <Input.TextArea rows={2} placeholder={t`Address`} />
+                </Form.Item>
+              </Col>
+            </Row>
+          </Card>
 
-          <Section><Trans>Purchasing</Trans></Section>
-          <Form.Item name="defaultCurrency" label={<Trans>Default currency</Trans>}>
-            <Select placeholder={t`Default currency`} allowClear showSearch>
-              {map(currencies, (currency) => (
-                <Select.Option value={currency} key={currency}>
-                  {currency}
-                </Select.Option>
-              ))}
-            </Select>
-          </Form.Item>
-          <Form.Item name="paymentTermsDays" label={<Trans>Payment terms (days)</Trans>}>
-            <InputNumber placeholder={t`Payment terms (days)`} min={0} precision={0} style={{ width: "100%" }} />
-          </Form.Item>
-
-          <Section><Trans>Address</Trans></Section>
-          <Form.Item name="address" label={<Trans>Address</Trans>}>
-            <Input.TextArea rows={4} placeholder={t`Address`} />
-          </Form.Item>
+          <Card title={<Trans>Purchasing</Trans>} style={{ marginBottom: 16 }}>
+            <Row gutter={[16, 0]}>
+              <Col xs={24} md={12}>
+                <Form.Item name="defaultCurrency" label={<Trans>Default currency</Trans>}>
+                  <Select placeholder={t`Default currency`} allowClear showSearch>
+                    {map(currencies, (currency) => (
+                      <Select.Option value={currency} key={currency}>
+                        {currency}
+                      </Select.Option>
+                    ))}
+                  </Select>
+                </Form.Item>
+              </Col>
+              <Col xs={24} md={12}>
+                <Form.Item name="paymentTermsDays" label={<Trans>Payment terms (days)</Trans>}>
+                  <InputNumber
+                    placeholder={t`Payment terms (days)`}
+                    min={0}
+                    precision={0}
+                    style={{ width: "100%" }}
+                  />
+                </Form.Item>
+              </Col>
+            </Row>
+          </Card>
         </Form>
       </ScrollShadow>
     </Drawer>
