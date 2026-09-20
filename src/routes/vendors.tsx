66976bbdbbe87652f1a48cdfa64aa81ce78d1a43
Mohamed Ali Missaoui <mam@missaoui.org>
2026-09-20 16:20:59 +0100

    feat: drop the Website list column, add CIN to the Clients list
    The Clients and Vendors lists no longer show a Website column; the Clients
    list now ends with an Identity number (CIN) column, the same field the Cash
    Book searches on.

--- src/routes/vendors.tsx
+++ src/routes/vendors.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useMemo, useState } from "react";
 import type { Vendor } from "src/types/models";
 import { Link, Outlet, useLocation, useNavigate } from "react-router";
-import { Button, Col, Empty, Space, Table, Row, Tag, Tooltip } from "antd";
+import { Button, Col, Empty, Space, Table, Row, Tag } from "antd";
 import { useAtomValue, useSetAtom } from "jotai";
 import { Trans } from "@lingui/react/macro";
 import { t } from "@lingui/core/macro";
 import { useLingui } from "@lingui/react";
-import { PhoneOutlined, SolutionOutlined, GlobalOutlined } from "@ant-design/icons";
+import { PhoneOutlined, SolutionOutlined } from "@ant-design/icons";
 import isEmpty from "lodash/isEmpty";
 import filter from "lodash/filter";
 import get from "lodash/get";
@@ -179,28 +179,6 @@ const Vendors = () => {
               dataIndex="vatin"
               key="vatin"
               sorter={(a: Vendor, b: Vendor) => (a.vatin ?? "").localeCompare(b.vatin ?? "")}
-            />
-            <Table.Column
-              title={<Trans>Website</Trans>}
-              dataIndex="website"
-              key="website"
-              width={60}
-              align="center"
-              sorter={(a: Vendor, b: Vendor) => (a.website ?? "").localeCompare(b.website ?? "")}
-              render={(website) =>
-                website ? (
-                  <Tooltip title={website}>
-                    <a
-                      href={website}
-                      target="_blank"
-                      rel="noreferrer noopener"
-                      onClick={(e) => e.stopPropagation()}
-                    >
-                      <GlobalOutlined style={{ fontSize: 16 }} />
-                    </a>
-                  </Tooltip>
-                ) : null
-              }
             />
           </Table>
           <Outlet />
